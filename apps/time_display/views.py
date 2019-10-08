0010e2709fa2a131ea487c7be43bc1c2a51794b6
Bridger Middleton <bmiddy3@gmail.com>
2019-10-08 15:57:09 -0600

    second commit

--- apps/time_display/views.py
+++ apps/time_display/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from time import gmtime, strftime
 def index(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        "time": strftime("%b %d, %Y %H:%M %p", gmtime())
     }
     return render(request, "time_display/index.html", context)
 def timedis(request):
