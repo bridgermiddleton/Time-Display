967f57f1c58da762d3aa3dd8f02ebdc69f407009
Bridger Middleton <bmiddy3@gmail.com>
2019-10-08 15:50:45 -0600

    first commit

---
+++ apps/time_display/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from time import gmtime, strftime
+def index(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request, "time_display/index.html", context)
+def timedis(request):
+    return redirect("/")
+# Create your views here.
